ab5e18a61b4d76a1c1dda472316fac32edff2762
Сергей <rovnins@gmail.com>
2025-07-27 14:51:56 +0300

    финальная версия

--- javaScript.js
+++ javaScript.js
@@ -7,5 +7,21 @@ addToCardBucket.forEach(button => {
     button.addEventListener("click", () => {
         cartCount++;
         cartCounter.textContent = cartCount
-    } )
+    })
 });
+
+
+const burgerBtn = document.querySelector('.burger')
+const navMenu = document.querySelector('.nav__menu__list')
+const burgerAnimation1 = document.querySelector('.burgerAnimation1')
+const burgerAnimation2 = document.querySelector('.burgerAnimation2')
+const burgerAnimation3 = document.querySelector('.burgerAnimation3')
+
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active')
+    burgerAnimation1.classList.toggle('active')
+    burgerAnimation2.classList.toggle('active')
+    burgerAnimation3.classList.toggle('active')
+
+})
